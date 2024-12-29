b3e51021e8e7b80c78e30e1bd36e42c1cac1f748
axelj123 <axeljhosmell13@gmail.com>
2024-12-28 23:16:49 -0500

    change images

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,38 @@ const nextConfig = {
   },
 
   compress: true,
-
   images: {
-    domains: ['example.com', 'avatars.githubusercontent.com','github.com','user-images.githubusercontent.com','i.giphy.com'], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/user-attachments/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.giphy.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        pathname: '/**',
+      },
+    ],
     formats: ['image/avif', 'image/webp'],
   },
 
   reactStrictMode: true,
-
 };
 
 export default nextConfig;
